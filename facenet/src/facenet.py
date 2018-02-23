966dc3e9608e9d4f638e0177932970f63c14e491
Alex Gabourie <agabourie47@gmail.com>
2018-02-23 13:20:31 -0800

    building small training set of nick and retta

--- facenet/src/facenet.py
+++ facenet/src/facenet.py
@@ -326,8 +326,10 @@ class ImageClass():
 def get_dataset(path, has_class_directories=True):
     dataset = []
     path_exp = os.path.expanduser(path)
-    classes = [path for path in os.listdir(path_exp) \
-                    if os.path.isdir(os.path.join(path_exp, path))]
+    classes = list()
+    if os.path.isdir(os.path.join(path_exp, path)):
+        for path in os.listdir(path_exp):
+            classes.append(path)
     classes.sort()
     nrof_classes = len(classes)
     for i in range(nrof_classes):
