ec88588d2d91151477b5cb519a578fe417d030be
AlexGabourie <agabourie47@gmail.com>
2018-03-14 18:37:43 -0700

    tried to create auto-dataset. Could not fetch character info

--- facenet/src/facenet.py
+++ facenet/src/facenet.py
@@ -327,9 +327,9 @@ def get_dataset(path, has_class_directories=True):
     dataset = []
     path_exp = os.path.expanduser(path)
     classes = list()
-    if os.path.isdir(os.path.join(path_exp, path)):
-        for path in os.listdir(path_exp):
-            classes.append(path)
+    #if os.path.isdir(os.path.join(path_exp, path)):
+    for path in os.listdir(path_exp):
+        classes.append(path)
     classes.sort()
     nrof_classes = len(classes)
     for i in range(nrof_classes):
